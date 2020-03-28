741bfbbccf953fee617f4869361bcd354f3ad8de
tatsuyafukui <nakedtatsuya@gmail.com>
2020-03-29 03:50:01 +0900

    fix bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     'gatsby-transformer-typescript-css-modules',
-    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
