afd288102dd68db8f11a87e3a01241252132fd39
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-06 20:26:28 +0900

    add style sceneList

--- src/components/Organisms/SceneList/index.js
+++ src/components/Organisms/SceneList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./styles.module.scss";
 import Img from "gatsby-image";
 import Heading from "../../Atoms/Heading";
 import MediaObjectLayout from "../../Atoms/MediaObjectLayout";
@@ -6,11 +7,11 @@ import MediaObjectLayout from "../../Atoms/MediaObjectLayout";
 const SceneList = ({ scenes }) => (
   <div>
     {scenes.map(scene => (
-      <MediaObjectLayout key={scene.id}>
+      <MediaObjectLayout key={scene.id} className={styles.root}>
         <div>
           <Img fixed={scene.image.childImageSharp.fixed} />
         </div>
-        <div style={{ padding: "32px" }}>
+        <div className={styles.right}>
           <Heading level={3}>{scene.id}</Heading>
           <Heading level={2}>{scene.heading}</Heading>
           <p>詳細を見る</p>
