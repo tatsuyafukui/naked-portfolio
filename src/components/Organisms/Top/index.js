f2db61073f3497fcdc842611fd354ccf2acd61cd
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-06 14:25:00 +0900

    fix top component

--- src/components/Organisms/Top/index.js
+++ src/components/Organisms/Top/index.js
@@ -4,21 +4,17 @@ import styles from './styles.module.scss';
 import Heading from "../../Atoms/Heading";
 import Txt from "../../Atoms/Txt";
 
-const Top = ({topImage}) => {
-
-		return (
-				<div className={styles.root}>
-						<div className={styles.container}>
-								<div className={styles.topSectionLeft}>
-										<Heading level={1}>目標へ向かう学習を</Heading>
-										<Txt>Journeyはあなたが自信をもって目標へ向かうための学習ロードマップです</Txt>
-								</div>
-								<div className={styles.topSectionRight}>
-										<Img fixed={topImage} />
-								</div>
+const Top = ({topImage}) => (
+		<div className={styles.root}>
+				<div className={styles.container}>
+						<div className={styles.topSectionLeft}>
+								<Heading level={1}>目標へ向かう学習を</Heading>
+								<Txt>Journeyはあなたが自信をもって目標へ向かうための学習ロードマップです</Txt>
+						</div>
+						<div className={styles.topSectionRight}>
+								<Img fixed={topImage} />
 						</div>
 				</div>
-		);
-};
-
+		</div>
+)
 export default Top;
