a9c4b96e69fb956915f9ed2ef1fc30e85b8a1966
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-08 18:59:14 +0900

    clean never used file

--- src/components/Templates/404Template/index.js
+++ src/components/Templates/404Template/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import SEO from "../../seo";
-import HolyGrailLayout, { HolyGrailTop, HolyGrailBottom, HolyGrailMain } from "../../Atoms/HolyGrailLayout";
 import Header from "../../Organisms/Header";
 
 const NotFoundTemplate = () => {
   return (
-    <HolyGrailLayout>
-      <HolyGrailTop>
-        <SEO title="404: Not found" />
-        <Header />
-      </HolyGrailTop>
-      <HolyGrailMain>
-        <main>
-          <h1>NOT FOUND</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        </main>
-      </HolyGrailMain>
-      <HolyGrailBottom>
-        <footer style={{ minHeight: "50px", backgroundColor: "blue" }}>footer</footer>
-      </HolyGrailBottom>
-    </HolyGrailLayout>
+    <div>
+      <SEO title="404: Not found" />
+      <Header />
+      <main>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </main>
+      <footer style={{ minHeight: "50px", backgroundColor: "blue" }}>footer</footer>
+    </div>
   );
 };
 
--- src/components/Templates/IndexTemplate/index.js
+++ src/components/Templates/IndexTemplate/index.js
@@ -1,30 +1,10 @@
 import React from "react";
-import styles from "./styles.module.scss";
-import SEO from "../../seo";
-import HolyGrailLayout, { HolyGrailTop, HolyGrailBottom, HolyGrailMain } from "../../Atoms/HolyGrailLayout";
-import Header from "../../Organisms/Header";
-import Top from "../../Organisms/Top";
-import SceneList from "../../Organisms/SceneList";
 
 const IndexTemplate = ({ topImage, scenes }) => {
   return (
-    <HolyGrailLayout>
-      <HolyGrailTop>
-        <SEO title={""} description={""} />
-        <Header />
-        <Top topImage={topImage} />
-      </HolyGrailTop>
-      <HolyGrailMain>
-        <main className={styles.main}>
-          <div className={styles.container}>
-            <SceneList scenes={scenes} />
-          </div>
-        </main>
-      </HolyGrailMain>
-      <HolyGrailBottom>
-        <footer style={{ minHeight: "50px", backgroundColor: "blue" }}>footer</footer>
-      </HolyGrailBottom>
-    </HolyGrailLayout>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
 };
 
