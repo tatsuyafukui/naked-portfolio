2e7ab74c29e65e76d497cd24bbb6d10128423b8f
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-09 17:59:36 +0900

    MediaObjectLayoutをOGPで使うレイアウトに対応するように修正

--- src/components/Atoms/MediaObjectLayout/index.js
+++ src/components/Atoms/MediaObjectLayout/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
-const MediaObjectLayout = ({ children, className, tag: Tag }) => {
+const MediaObjectLayout = ({ tag: Tag, summary, children, className }) => {
   return (
-    <Tag className={[styles.root, className].join(" ")}>
+    <Tag className={[styles.root, styles[summary], className].join(" ")}>
       <div>{children[0]}</div>
       <div className={styles.body}>{children.slice(1)}</div>
     </Tag>
@@ -12,5 +12,7 @@ const MediaObjectLayout = ({ children, className, tag: Tag }) => {
 
 MediaObjectLayout.defaultProps = {
   tag: "div",
+  summary: "left",
 };
+
 export default MediaObjectLayout;
