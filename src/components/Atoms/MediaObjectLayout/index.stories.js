e5e25e23848a2f28093756a19fa2fb3f40dbe372
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-09 14:09:01 +0900

    不要な変更削除

--- src/components/Atoms/MediaObjectLayout/index.stories.js
+++ src/components/Atoms/MediaObjectLayout/index.stories.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import MediaObjectLayout from "./index";
+const dammy = require("../../../mock/images/img01.png");
 
 storiesOf(`Atoms/MediaObjectLayout`, module)
   .add(`デフォルト`, () => (
     <MediaObjectLayout>
       <div>
+        <img src={dammy} width={300} height={300} />
       </div>
       <p>説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト</p>
       <p>説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト</p>
@@ -14,6 +16,7 @@ storiesOf(`Atoms/MediaObjectLayout`, module)
   .add(`sectionタグ指定`, () => (
     <MediaObjectLayout tag={"section"}>
       <div>
+        <img src={dammy} width={300} height={300} />
       </div>
       <p>説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト</p>
     </MediaObjectLayout>
