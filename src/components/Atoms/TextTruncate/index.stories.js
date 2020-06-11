381424dc6e0d6e563b944e40db5eacafbbe62bb3
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-11 15:20:07 +0900

    命名変更＆Lintした

--- src/components/Atoms/TextTruncate/index.stories.js
+++ src/components/Atoms/TextTruncate/index.stories.js
@@ -1,43 +1,43 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Overwrap from "./index";
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import TextTruncate from './index'
 
-storiesOf(`Atoms/Overwrap`, module)
+storiesOf(`Atoms/TextTruncate`, module)
   .add(`Default`, () => (
-    <div style={{ width: "300px", height: "100px" }}>
+    <div style={{width: '300px', height: '100px'}}>
       <p>
-        <Overwrap>
+        <TextTruncate>
           「自分だけのWebページを作ってみたい。」そう思ったことはありませんか。このシーンでは、Webページを作るために必要なスキルを説明しています。
           学習を始めたばかりのころは、見慣れない文法や知らない単語が多く、難しいと感じることもあるかもしれません。しかし、基礎的なスキルから着実に学んでいけば、きっと作れるようになります。まずはシンプルなレイアウトのWebページを作れるようになりましょう。
           このシーンのスキルを学び終えたとき、あなたはWebページの作成と公開ができるようになっています。Webページが公開されるその日を、私たちは楽しみにしています。
-        </Overwrap>
+        </TextTruncate>
       </p>
     </div>
   ))
   .add(`Open is true`, () => (
-    <div style={{ width: "300px", height: "100px" }}>
-      <Overwrap open>
+    <div style={{width: '300px', height: '100px'}}>
+      <TextTruncate open>
         「自分だけのWebページを作ってみたい。」そう思ったことはありませんか。このシーンでは、Webページを作るために必要なスキルを説明しています。
         学習を始めたばかりのころは、見慣れない文法や知らない単語が多く、難しいと感じることもあるかもしれません。しかし、基礎的なスキルから着実に学んでいけば、きっと作れるようになります。まずはシンプルなレイアウトのWebページを作れるようになりましょう。
         このシーンのスキルを学び終えたとき、あなたはWebページの作成と公開ができるようになっています。Webページが公開されるその日を、私たちは楽しみにしています。
-      </Overwrap>
+      </TextTruncate>
     </div>
   ))
-  .add(`textOverflow is "read more"`, () => (
-    <div style={{ width: "300px", height: "100px" }}>
-      <Overwrap textOverflow={"... 続きを読む"}>
+  .add(`truncateText is "read more"`, () => (
+    <div style={{width: '300px', height: '100px'}}>
+      <TextTruncate truncateText={'... 続きを読む'}>
         「自分だけのWebページを作ってみたい。」そう思ったことはありませんか。このシーンでは、Webページを作るために必要なスキルを説明しています。
         学習を始めたばかりのころは、見慣れない文法や知らない単語が多く、難しいと感じることもあるかもしれません。しかし、基礎的なスキルから着実に学んでいけば、きっと作れるようになります。まずはシンプルなレイアウトのWebページを作れるようになりましょう。
         このシーンのスキルを学び終えたとき、あなたはWebページの作成と公開ができるようになっています。Webページが公開されるその日を、私たちは楽しみにしています。
-      </Overwrap>
+      </TextTruncate>
     </div>
   ))
-  .add(`textOverflow is JSX`, () => (
-    <div style={{ width: "300px", height: "100px" }}>
-      <Overwrap textOverflow={<button>... 続きを読む</button>}>
+  .add(`truncateText is JSX`, () => (
+    <div style={{width: '300px', height: '100px'}}>
+      <TextTruncate truncateText={<button>... 続きを読む</button>}>
         「自分だけのWebページを作ってみたい。」そう思ったことはありませんか。このシーンでは、Webページを作るために必要なスキルを説明しています。
         学習を始めたばかりのころは、見慣れない文法や知らない単語が多く、難しいと感じることもあるかもしれません。しかし、基礎的なスキルから着実に学んでいけば、きっと作れるようになります。まずはシンプルなレイアウトのWebページを作れるようになりましょう。
         このシーンのスキルを学び終えたとき、あなたはWebページの作成と公開ができるようになっています。Webページが公開されるその日を、私たちは楽しみにしています。
-      </Overwrap>
+      </TextTruncate>
     </div>
-  ));
+  ))
