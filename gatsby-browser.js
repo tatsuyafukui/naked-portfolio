7606825e5ebecbeaeeed018d8b241466055284cd
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-15 19:27:28 +0900

    gatsby-browserの不要なコメント削除

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 import './src/styles/global.css'
 import {config} from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
