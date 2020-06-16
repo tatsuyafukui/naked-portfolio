bee50348aa56e3771aaa29f0162c39ca9db0a5d6
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-16 13:49:46 +0900

    rebase　したコードのフォーマット修正

--- src/components/Organisms/Footer/index.js
+++ src/components/Organisms/Footer/index.js
@@ -10,47 +10,45 @@ import {DisableTxt} from '../../Atoms/Txt'
 const FooterList = () => {
   const links = [
     {
-      'label': 'Progate',
-      'url': 'https://prog-8.com/'
+      label: 'Progate',
+      url: 'https://prog-8.com/',
     },
     {
-      'label': '運営会社',
-      'url': 'https://prog-8.com/about'
+      label: '運営会社',
+      url: 'https://prog-8.com/about',
     },
     {
-      'label': '利用規約',
-      'url': 'https://prog-8.com/policy'
+      label: '利用規約',
+      url: 'https://prog-8.com/policy',
     },
     {
-      'label': 'プライバシーポリシー',
-      'url': 'https://prog-8.com/privacy_policy'
+      label: 'プライバシーポリシー',
+      url: 'https://prog-8.com/privacy_policy',
     },
     {
-      'label': 'Twitter',
-      'url': 'https://twitter.com/progateJP'
+      label: 'Twitter',
+      url: 'https://twitter.com/progateJP',
     },
     {
-      'label': 'Facebook',
-      'url': 'https://www.facebook.com/ProgateOfficial/'
-    }
+      label: 'Facebook',
+      url: 'https://www.facebook.com/ProgateOfficial/',
+    },
   ]
 
   return (
     <List>
-      {
-        links.map((link) => (
-          <ListItem className={styles.item} key={link.label}>
-            <Link
-              to={link.url}
-              target='_blank'
-              rel="noopener noreferrer"
-              className={styles.link}
-            >
-              {link.label}
-            </Link>
-          </ListItem>
-        ))
-      }
+      {links.map(link => (
+        <ListItem className={styles.item} key={link.label}>
+          <Link
+            to={link.url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.link}
+          >
+            {link.label}
+          </Link>
+        </ListItem>
+      ))}
     </List>
   )
 }
@@ -63,10 +61,7 @@ const Footer = className => (
       </Link>
       <div className={styles.inner}>
         <FooterList />
-        <DisableTxt
-          fontSize='xxs'
-          className={styles.copyright}
-        >
+        <DisableTxt fontSize='xxs' className={styles.copyright}>
           © 2020 Progate, Inc.
         </DisableTxt>
       </div>
