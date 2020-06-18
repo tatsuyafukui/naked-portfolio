6eee8ba10814f4b90e4074bc1df84c4e3d44df79
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-18 22:29:10 +0900

    create scene template

--- src/components/Templates/SceneTemplate/index.js
+++ src/components/Templates/SceneTemplate/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Img from 'gatsby-image'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faAngleRight} from '@fortawesome/free-solid-svg-icons'
 import styles from './styles.module.scss'
@@ -6,54 +7,71 @@ import SEO from '../../seo'
 import Breadcrumb from '../../Atoms/Breadcrumb'
 import Container from '../../Atoms/Container'
 import Link from '../../Atoms/Link'
-import Txt from '../../Atoms/Txt'
+import Txt, {InfoTxt} from '../../Atoms/Txt'
+import TextTruncate from '../../Atoms/TextTruncate'
 import Heading from '../../Atoms/Heading'
 import Header from '../../Organisms/Header'
 import Footer from '../../Organisms/Footer'
 import SkillList from '../../Organisms/SkillList'
 
-const SceneTemplate = ({scene, skills}) => {
-  return (
-    <div>
-      <SEO title={scene.title} description={scene.description} lang={'ja'} />
-      <Header />
-      <main className={styles.main}>
-        <Container>
+const SceneTemplate = ({scene, skills, open, onClick}) => (
+  <div>
+    <SEO title={scene.title} description={scene.description} lang={'ja'} />
+    <Header />
+    <main className={styles.main}>
+      <Container>
+        <div>
           <div>
-            <div>
-              <Breadcrumb
-                className={styles.breadcrumb}
-                separator={<FontAwesomeIcon icon={faAngleRight} />}
+            <Breadcrumb
+              className={styles.breadcrumb}
+              separator={<FontAwesomeIcon icon={faAngleRight} />}
+            >
+              <Link to='/'>
+                <Txt>Home</Txt>
+              </Link>
+              <Txt>{scene.id}</Txt>
+            </Breadcrumb>
+            <Heading level={1} className={styles.title}>
+              {scene.title}
+            </Heading>
+          </div>
+          <div>
+            <Img
+              className={styles.visual}
+              fluid={scene.image.childImageSharp.fluid}
+              alt={scene.title}
+            />
+          </div>
+        </div>
+        <div className={styles.body}>
+          <div>
+            {/* PC */}
+            <Txt className={styles.descriptionPc}>{scene.description}</Txt>
+            {/* SP */}
+            <Txt className={styles.descriptionSp}>
+              <TextTruncate
+                open={open}
+                truncateText={
+                  <button className={styles.button} onClick={onClick}>
+                    <InfoTxt>...続きを読む</InfoTxt>
+                  </button>
+                }
               >
-                <Link to='/'>
-                  <Txt>Home</Txt>
-                </Link>
-                <Txt>{scene.id}</Txt>
-              </Breadcrumb>
-              <Heading level={1} className={styles.title}>
-                {scene.title}
-              </Heading>
-            </div>
-            <div className={styles.visual}>
-              <img src={scene.image} alt={scene.title} />
-            </div>
+                {scene.description}
+              </TextTruncate>
+            </Txt>
           </div>
-          <div className={styles.body}>
-            <div>
-              <Txt>{scene.description}</Txt>
-            </div>
-            <div>
-              <Heading level={4} className={styles.requireLabel}>
-                必要なスキル
-              </Heading>
-              <SkillList skills={skills} />
-            </div>
+          <div>
+            <Heading level={4} className={styles.requireLabel}>
+              必要なスキル
+            </Heading>
+            <SkillList skills={skills} />
           </div>
-        </Container>
-      </main>
-      <Footer />
-    </div>
-  )
-}
+        </div>
+      </Container>
+    </main>
+    <Footer />
+  </div>
+)
 
 export default SceneTemplate
--- src/components/Templates/SceneTemplate/index.stories.js
+++ src/components/Templates/SceneTemplate/index.stories.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
+import {action} from '@storybook/addon-actions'
 import SceneTemplate from './index'
-import sceneData from '../../../mock/data/scenes'
 import skillsData from '../../../mock/data/skills'
-import sceneImage from '../../../mock/images/scene.png'
-sceneData[0].image = sceneImage
+import data from '../../../mock/data/scene'
 
-storiesOf('Template/SceneTemplate', module).add('Default', () => (
-  <SceneTemplate scene={sceneData[0]} skills={skillsData} />
-))
+storiesOf('Template/SceneTemplate', module)
+  .add('Default - PC', () => (
+    <SceneTemplate
+      scene={data.scenesJson}
+      skills={skillsData}
+      open={false}
+      onClick={action('Read more was clicked')}
+    />
+  ))
+  .add('SP - open is false', () => (
+    <SceneTemplate
+      scene={data.scenesJson}
+      skills={skillsData}
+      open={false}
+      onClick={action('Read more was clicked')}
+    />
+  ))
+  .add('SP - open is true', () => (
+    <SceneTemplate
+      scene={data.scenesJson}
+      skills={skillsData}
+      open={true}
+      onClick={action('Read more was clicked')}
+    />
+  ))
