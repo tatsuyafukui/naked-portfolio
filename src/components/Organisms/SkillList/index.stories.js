5151921e67990a9de327a470bcd9a97460e509ef
tatsuya fukuigmail.com <nakedtatsuya@gmail.com>
2020-06-18 13:39:12 +0900

    format

--- src/components/Organisms/SkillList/index.stories.js
+++ src/components/Organisms/SkillList/index.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
 import SkillList from './index'
-import skills from '../../../mock/data/skills'
+import skills from '../../../mock/data/skills.json'
 
 storiesOf('Organisms/SkillList', module).add('Default', () => (
   <SkillList skills={skills} />
