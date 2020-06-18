f7b7e411452969a7a2f7c9642a4a1f758ff0e54d
kuon609 <kuon@prog-8.com>
2020-06-18 13:26:33 +0900

    add bold heading

--- src/components/Atoms/Heading/index.js
+++ src/components/Atoms/Heading/index.js
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
 import {containPresenter} from '../../utils/HoC'
 
-const HeadingPresenter = ({tag: Tag, visualLevel, className, ...props}) => (
+const headingFactory = role => ({tag: Tag, visualLevel, className, ...props}) => (
   <Tag
-    className={[styles.h, styles[`h${visualLevel}`], className].join(' ')}
+    className={[styles.h, styles[role], styles[`h${visualLevel}`], className].join(' ')}
     {...props}
   />
 )
+
+const HeadingPresenter = headingFactory('normal')
+const BoldHeadingPresenter = headingFactory('bold')
 
 export const HeadingContainer = ({visualLevel, level, presenter, ...props}) => {
   level = Math.max(1, Math.min(6, level))
@@ -21,6 +24,7 @@ export const HeadingContainer = ({visualLevel, level, presenter, ...props}) => {
 }
 
 const Heading = containPresenter(HeadingContainer, HeadingPresenter)
+export const BoldHeading = containPresenter(HeadingContainer, BoldHeadingPresenter)
 
 Heading.propTypes = {
   children: PropTypes.node.isRequired,
@@ -29,6 +33,16 @@ Heading.propTypes = {
 }
 
 Heading.defaultProps = {
+  level: 2,
+}
+
+BoldHeading.propTypes = {
+  children: PropTypes.node.isRequired,
+  level: PropTypes.number,
+  visualLevel: PropTypes.number,
+}
+
+BoldHeading.defaultProps = {
   level: 2,
 }
 
