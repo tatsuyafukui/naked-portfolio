d4b6b0d01bfab0ddee2aad45a25072b7a9fc6796
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-23 21:30:43 +0900

    add contents

--- src/components/Templates/SkillTemplate/index.js
+++ src/components/Templates/SkillTemplate/index.js
@@ -16,6 +16,7 @@ import Header from '../../Organisms/Header'
 import Footer from '../../Organisms/Footer'
 import SkillList from '../../Organisms/SkillList'
 import {graphql, useStaticQuery} from "gatsby";
+import List, {ListItem} from "../../Atoms/List";
 
 const SkillTemplate = () => {
   const data = useStaticQuery(graphql`
@@ -32,6 +33,10 @@ const SkillTemplate = () => {
           overall
           overview
           purpose
+          standard {
+            basic
+            practical
+          }
           slug
           scene {
             id
@@ -74,19 +79,72 @@ const SkillTemplate = () => {
         <div>
           <Container className={styles.container}>
             <div>
-              <Overlay>
-                <Front><Heading>{skill.title}</Heading></Front>
-                <Back><Img fluid={skill.scene.image.childImageSharp.fluid} /></Back>
+              <Overlay className={styles.visual} overlayStyle={{backgroundColor: "rgba(3, 0, 39, 0.39)"}}>
+                <Front>
+                  <BoldHeading level={1} className={styles.title}>
+                    {skill.title}
+                  </BoldHeading>
+                </Front>
+                <Back>
+                  <Img
+                    fluid={skill.scene.image.childImageSharp.fluid}
+                  />
+                </Back>
               </Overlay>
             </div>
 
-            <div>
-              {skill.overview}
+            <div className={styles.overview}>
+              <Txt dangerouslySetInnerHTML={{ __html: skill.overview }}/>
             </div>
 
-            <div>
+            <div className={styles.image}>
               <Img fluid={skill.image.childImageSharp.fluid} />
             </div>
+
+            <div className={styles.purpose}>
+              <BoldHeading level={3}>
+                学ぶ目的
+              </BoldHeading>
+              <Txt dangerouslySetInnerHTML={{ __html: skill.purpose }}/>
+            </div>
+
+            <div className={styles.learning}>
+              <Heading level={2}>
+                学んでみよう
+              </Heading>
+              <div className={styles.standard}>
+                <BoldHeading level={3}>
+                  学習の目安
+                </BoldHeading>
+                <div>
+                  <BoldHeading level={4}>
+                    基礎
+                  </BoldHeading>
+                  <List>
+                    {skill.standard.basic.map(item => (
+                      <ListItem><Txt>{item}</Txt></ListItem>
+                    ))}
+                  </List>
+                </div>
+                <div>
+                  <BoldHeading level={4}>
+                    応用
+                  </BoldHeading>
+                  <List>
+                    {skill.standard.practical.map(item => (
+                      <ListItem key={item}><Txt>{item}</Txt></ListItem>
+                    ))}
+                  </List>
+                </div>
+              </div>
+            </div>
+            <div className={styles.overall}>
+              <BoldHeading level={3}>
+                全体像をつかむには
+              </BoldHeading>
+              <Txt dangerouslySetInnerHTML={{ __html: skill.overall }}/>
+            </div>
+
           </Container>
         </div>
       </main>
