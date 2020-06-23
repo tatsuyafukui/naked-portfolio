fab716981a3b46b2fef73d0b85fd357535d507c8
kuon609 <kuon@prog-8.com>
2020-06-23 18:46:05 +0900

    modify typography

--- src/components/Atoms/Txt/index.js
+++ src/components/Atoms/Txt/index.js
@@ -1,41 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
+import {containPresenter} from '../../utils/HoC'
 
 const txtFactory = role => ({
   tag: Tag = 'p',
-  fontSize = 'm',
+  visualLevel,
   className,
   ...props
 }) => (
   <Tag
     {...props}
-    className={[styles.txt, styles[role], styles[fontSize], className].join(
+    className={[styles.txt, styles[role], styles[`visualLevel${visualLevel}`], className].join(
       ' '
     )}
   />
 )
 
-const Txt = txtFactory('base')
-export const InfoTxt = txtFactory('info')
-export const DisableTxt = txtFactory('disable')
+const TxtPresenter = txtFactory('base')
+const InfoTxtPresenter = txtFactory('info')
+const DisableTxtPresenter = txtFactory('disable')
+
+export const TxtContainer = ({visualLevel, presenter, ...props}) => {
+  visualLevel = Math.max(1, Math.min(4, visualLevel))
+  return presenter({visualLevel, ...props})
+}
+
+const Txt = containPresenter(TxtContainer, TxtPresenter)
+export const InfoTxt = containPresenter(TxtContainer, InfoTxtPresenter)
+export const DisableTxt = containPresenter(TxtContainer, DisableTxtPresenter)
 
 Txt.propTypes = {
   children: PropTypes.node.isRequired,
   tag: PropTypes.string,
-  fontSize: PropTypes.string,
+  visualLevel: PropTypes.number,
+}
+
+Txt.defaultProps = {
+  visualLevel: 1,
 }
 
 InfoTxt.propTypes = {
   children: PropTypes.node.isRequired,
   tag: PropTypes.string,
-  fontSize: PropTypes.string,
+  visualLevel: PropTypes.number,
+}
+
+InfoTxt.defaultProps = {
+  visualLevel: 1,
 }
 
 DisableTxt.propTypes = {
   children: PropTypes.node.isRequired,
   tag: PropTypes.string,
-  fontSize: PropTypes.string,
+  visualLevel: PropTypes.number,
+}
+
+DisableTxt.defaultProps = {
+  visualLevel: 1,
 }
 
 export default Txt
