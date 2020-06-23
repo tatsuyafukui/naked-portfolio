27cb246204059e9b44ac66fb105ba2e8e70c6fb2
kuon609 <kuon@prog-8.com>
2020-06-23 11:08:24 +0900

    add story

--- src/components/Templates/IndexTemplate/index.js
+++ src/components/Templates/IndexTemplate/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
 import Header from '../../Organisms/Header'
 import Footer from '../../Organisms/Footer'
 import MainVisual from '../../Organisms/MainVisual'
 import Container from '../../Atoms/Container'
 import List, {ListItem} from '../../Atoms/List'
-import MediaObjectLayout from '../../Atoms/MediaObjectLayout'
-import Link from '../../Atoms/Link'
-import Heading from '../../Atoms/Heading'
-import {InfoTxt} from '../../Atoms/Txt'
+import SceneLink from '../../Organisms/SceneLink'
 
-const IndexTemplate = ({topImage, scenes}) => {
+const IndexTemplate = ({scenes}) => {
   return (
     <>
       <Header />
@@ -19,17 +15,11 @@ const IndexTemplate = ({topImage, scenes}) => {
         <MainVisual />
         <Container className={styles.container}>
           <List>
-            <ListItem className={styles.item}>
-              <Link to='/'>
-                <MediaObjectLayout>
-                  <img src="https://placehold.jp/440x293.png" alt="placeholder"/>
-                  <div className={styles.titleWrap}>
-                    <InfoTxt fontSize='xl' className={styles.number}>シーン1</InfoTxt>
-                    <Heading level={2}>Webページを作れるようになろう</Heading>
-                  </div>
-                </MediaObjectLayout>
-              </Link>
-            </ListItem>
+            {scenes.map(scene => (
+              <ListItem className={styles.item}>
+                <SceneLink scene={scene} />
+              </ListItem>
+            ))}
           </List>
         </Container>
       </main>
---
+++ src/components/Templates/IndexTemplate/index.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import IndexTemplate from './index'
+import data from '../../../mock/data/scene'
+
+
+storiesOf('Template/IndexTemplate', module).add('Default', () => (
+  <IndexTemplate
+    scenes={data.allScenesJson.nodes}
+  />
+))
