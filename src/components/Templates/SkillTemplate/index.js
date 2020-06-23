7f2701b017caed972eac9733cd91b981b89ced99
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-23 21:33:47 +0900

    fix format

--- src/components/Templates/SkillTemplate/index.js
+++ src/components/Templates/SkillTemplate/index.js
@@ -15,56 +15,56 @@ import Heading, {BoldHeading} from '../../Atoms/Heading'
 import Header from '../../Organisms/Header'
 import Footer from '../../Organisms/Footer'
 import SkillList from '../../Organisms/SkillList'
-import {graphql, useStaticQuery} from "gatsby";
-import List, {ListItem} from "../../Atoms/List";
+import {graphql, useStaticQuery} from 'gatsby'
+import List, {ListItem} from '../../Atoms/List'
 
 const SkillTemplate = () => {
   const data = useStaticQuery(graphql`
-      query {
-        skillsJson(id: {eq: "HTML&CSS"}) {
-          title
+    query {
+      skillsJson(id: {eq: "HTML&CSS"}) {
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+        overall
+        overview
+        purpose
+        standard {
+          basic
+          practical
+        }
+        slug
+        scene {
+          id
           image {
             childImageSharp {
-              fluid {
+              fluid(maxWidth: 1140) {
                 ...GatsbyImageSharpFluid_withWebp
               }
             }
           }
-          overall
-          overview
-          purpose
-          standard {
-            basic
-            practical
-          }
           slug
-          scene {
-            id
-            image {
-              childImageSharp {
-                fluid(maxWidth: 1140) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
-            }
-            slug
-          }
         }
       }
-    `)
+    }
+  `)
 
-  const skill = data.skillsJson;
+  const skill = data.skillsJson
 
   return (
     <div>
-      <SEO title={skill.title} description={skill.overview} lang={'ja'}/>
-      <Header/>
+      <SEO title={skill.title} description={skill.overview} lang={'ja'} />
+      <Header />
       <main className={styles.main}>
         <div>
           <Container>
             <Breadcrumb
               className={styles.breadcrumb}
-              separator={<FontAwesomeIcon icon={faAngleRight}/>}
+              separator={<FontAwesomeIcon icon={faAngleRight} />}
             >
               <Link to='/'>
                 <Txt>Home</Txt>
@@ -79,22 +79,23 @@ const SkillTemplate = () => {
         <div>
           <Container className={styles.container}>
             <div>
-              <Overlay className={styles.visual} overlayStyle={{backgroundColor: "rgba(3, 0, 39, 0.39)"}}>
+              <Overlay
+                className={styles.visual}
+                overlayStyle={{backgroundColor: 'rgba(3, 0, 39, 0.39)'}}
+              >
                 <Front>
                   <BoldHeading level={1} className={styles.title}>
                     {skill.title}
                   </BoldHeading>
                 </Front>
                 <Back>
-                  <Img
-                    fluid={skill.scene.image.childImageSharp.fluid}
-                  />
+                  <Img fluid={skill.scene.image.childImageSharp.fluid} />
                 </Back>
               </Overlay>
             </div>
 
             <div className={styles.overview}>
-              <Txt dangerouslySetInnerHTML={{ __html: skill.overview }}/>
+              <Txt dangerouslySetInnerHTML={{__html: skill.overview}} />
             </div>
 
             <div className={styles.image}>
@@ -102,53 +103,44 @@ const SkillTemplate = () => {
             </div>
 
             <div className={styles.purpose}>
-              <BoldHeading level={3}>
-                学ぶ目的
-              </BoldHeading>
-              <Txt dangerouslySetInnerHTML={{ __html: skill.purpose }}/>
+              <BoldHeading level={3}>学ぶ目的</BoldHeading>
+              <Txt dangerouslySetInnerHTML={{__html: skill.purpose}} />
             </div>
 
             <div className={styles.learning}>
-              <Heading level={2}>
-                学んでみよう
-              </Heading>
+              <Heading level={2}>学んでみよう</Heading>
               <div className={styles.standard}>
-                <BoldHeading level={3}>
-                  学習の目安
-                </BoldHeading>
+                <BoldHeading level={3}>学習の目安</BoldHeading>
                 <div>
-                  <BoldHeading level={4}>
-                    基礎
-                  </BoldHeading>
+                  <BoldHeading level={4}>基礎</BoldHeading>
                   <List>
                     {skill.standard.basic.map(item => (
-                      <ListItem><Txt>{item}</Txt></ListItem>
+                      <ListItem>
+                        <Txt>{item}</Txt>
+                      </ListItem>
                     ))}
                   </List>
                 </div>
                 <div>
-                  <BoldHeading level={4}>
-                    応用
-                  </BoldHeading>
+                  <BoldHeading level={4}>応用</BoldHeading>
                   <List>
                     {skill.standard.practical.map(item => (
-                      <ListItem key={item}><Txt>{item}</Txt></ListItem>
+                      <ListItem key={item}>
+                        <Txt>{item}</Txt>
+                      </ListItem>
                     ))}
                   </List>
                 </div>
               </div>
             </div>
             <div className={styles.overall}>
-              <BoldHeading level={3}>
-                全体像をつかむには
-              </BoldHeading>
-              <Txt dangerouslySetInnerHTML={{ __html: skill.overall }}/>
+              <BoldHeading level={3}>全体像をつかむには</BoldHeading>
+              <Txt dangerouslySetInnerHTML={{__html: skill.overall}} />
             </div>
-
           </Container>
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
