2a61c15df033081884beac3c16033e61a2c54f23
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-25 23:20:30 +0900

    add open-graph-scraper

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const ogs = require('open-graph-scraper')
 
-// You can delete this file if you're not using it
+exports.onCreateNode = async ({node, actions}) => {
+  const {createNodeField} = actions
+  if (node.internal.type === `RecommendedJson`) {
+    // URLからOGP情報を取得
+    const results = await ogs({url: node.url}, (error, results) => {
+      if (error) {
+        throw Error('OGP取得のURLが不適切です')
+      }
+      return results
+    })
+
+    // Amazonはogp画像が正常に設定されていないので、ISBNから画像URLを指定
+    if (node.isBook) {
+      results.ogImage = {
+        type: 'jpg',
+        url: `https://images-na.ssl-images-amazon.com/images/P/${node.ISBN}._SL500_.jpg`,
+      }
+    }
+
+    createNodeField({
+      node,
+      name: `ogp`,
+      value: results,
+    })
+  }
+}
