754b60f609a69d2f62c52fe50e209cbe0e4ad62e
tatsuyafukui <nakedtatsuya@gmail.com>
2020-06-26 13:20:40 +0900

    fix

--- src/components/Templates/SkillTemplate/index.stories.js
+++ src/components/Templates/SkillTemplate/index.stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 import SkillTemplate from './index'
 import data from '../../../mock/data/skill'
-console.log(data)
+
 storiesOf('Template/SkillTemplate', module).add('Default', () => (
   <SkillTemplate
     skill={data.skillsJson}
