72e4870b8ef66229714c24bd4762e3c0961d41c9
tatsuyafukui <nakedtatsuya@gmail.com>
2020-07-03 00:46:15 +0900

    fix format

--- src/components/Organisms/OgpList/index.stories.js
+++ src/components/Organisms/OgpList/index.stories.js
@@ -4,5 +4,8 @@ import OgpList from './index'
 import data from '../../../mock/data/recommended.json'
 
 storiesOf('Organisms/OgpList', module).add('Default', () => (
-  <OgpList ogpList={data.allRecommendedJson.nodes} style={{maxWidth: '800px'}} />
+  <OgpList
+    ogpList={data.allRecommendedJson.nodes}
+    style={{maxWidth: '800px'}}
+  />
 ))
