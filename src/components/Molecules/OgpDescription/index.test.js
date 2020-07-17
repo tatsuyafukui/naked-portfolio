c5f221aea25ad077f6a2a68749aba707f85ff4ce
kuon609 <kuon@prog-8.com>
2020-07-17 10:40:08 +0900

    remove console log

--- src/components/Molecules/OgpDescription/index.test.js
+++ src/components/Molecules/OgpDescription/index.test.js
@@ -5,13 +5,11 @@ import {mount} from 'enzyme'
 describe('OgpDescription', () => {
   it('descriptionが無いとき、InfoTxtを表示しない', () => {
     const wrapper = mount(<OgpDescription description={undefined} />)
-    console.log(wrapper.debug())
     expect(wrapper.find('p.description')).toHaveLength(0)
   })
 
   it('descriptionがある時、InfoTxtを表示する', () => {
     const wrapper = mount(<OgpDescription description={'description'} />)
-    console.log(wrapper.debug())
     expect(wrapper.find('p.description')).toHaveLength(1)
   })
 })
