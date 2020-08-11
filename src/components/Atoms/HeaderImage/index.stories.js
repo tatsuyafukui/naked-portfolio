a146169b989a3f75b73eb93618c22b8d04ad98b3
kuon609 <kuon@prog-8.com>
2020-08-11 19:12:32 +0900

    add gatsby-image

--- src/components/Atoms/HeaderImage/index.stories.js
+++ src/components/Atoms/HeaderImage/index.stories.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
 import HeroImage from './index'
-import data from '../../../mock/data/scene'
+import Img from 'gatsby-image'
+import scene from '../../../mock/data/scene'
+import skill from '../../../mock/data/skill.json'
 
-storiesOf('Atoms/HeroImage', module).add('Default', () => (
-  <HeroImage>
-    <img src={data.scenesJson.image.publicURL} alt={data.scenesJson.title} />
-  </HeroImage>
-))
+storiesOf('Atoms/HeroImage', module)
+  .add('Default', () => (
+    <HeroImage>
+      <img
+        src={scene.scenesJson.image.publicURL}
+        alt={scene.scenesJson.title}
+      />
+    </HeroImage>
+  ))
+  .add('with gatsby-image', () => (
+    <HeroImage>
+      <Img
+        fluid={skill.skillsJson.thumbnail.childImageSharp.fluid}
+        alt={skill.skillsJson.title}
+      />
+    </HeroImage>
+  ))
