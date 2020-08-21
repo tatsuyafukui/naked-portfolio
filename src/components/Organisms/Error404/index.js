27c17971b97787067ad8600540ea101c3b37b6cb
ruiru1227 <rui_maeshima@prog-8.com>
2020-08-21 17:48:07 +0900

    修正

--- src/components/Organisms/Error404/index.js
+++ src/components/Organisms/Error404/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import Heading from '../../Atoms/Heading'
+import Image from '../../../images/icon.png'
 
-const Error404 = () => <div className={styles.root}>404 Not found</div>
+const Error404 = () => (
+  <div className={styles.root}>
+    404 Not found
+    <Heading>aaaaa</Heading>
+    <img src={Image} />
+  </div>
+)
 
 export default Error404
