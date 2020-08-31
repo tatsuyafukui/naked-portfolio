befc83b4bda91b855e3f498bf9352fd3abbbe60d
ruiru1227 <rui_maeshima@prog-8.com>
2020-08-31 12:01:54 +0900

    “ButtonをonClickの記述に変更“

--- src/components/Organisms/Error404/index.js
+++ src/components/Organisms/Error404/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './styles.module.scss'
 import Heading from '../../Atoms/Heading'
 import Button from '../../Atoms/Button'
-import Link from '../../Atoms/Link'
 import {LongTxt} from '../../Atoms/Txt'
 import Cloudright from '../../../images/404/cloud_right.png'
 import Cloudleft from '../../../images/404/cloud_left.png'
@@ -10,6 +9,7 @@ import Wavefront from '../../../images/404/wave_front.png'
 import Waveback from '../../../images/404/wave_back.png'
 import Ship from '../../../images/404/ship.png'
 import Ballooning from '../../../images/404/ballooning.png'
+import {navigate} from 'gatsby'
 
 const Error404 = () => (
   <div className={styles.root}>
@@ -19,9 +19,9 @@ const Error404 = () => (
         Oh, Looks likes you’re lost at sea. <br />
         Try going back to the previous page.
       </LongTxt>
-      <Link to='/'>
-        <Button className={styles.home}>GO TO HOME</Button>
-      </Link>
+      <Button className={styles.home} onClick={() => navigate('/')}>
+        GO TO HOMEgit
+      </Button>
     </div>
     <img className={styles.ballooning} src={Ballooning} />
     <img className={styles.cloudleft} src={Cloudleft} />
