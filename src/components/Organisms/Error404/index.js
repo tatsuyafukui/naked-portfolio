e877eff72cdf4d7fb86240be9067829a684dee80
kuon609 <kuon@prog-8.com>
2020-09-02 16:36:49 +0900

    一旦コミット

--- src/components/Organisms/Error404/index.js
+++ src/components/Organisms/Error404/index.js
@@ -3,33 +3,45 @@ import styles from './styles.module.scss'
 import Heading from '../../Atoms/Heading'
 import Button from '../../Atoms/Button'
 import {LongTxt} from '../../Atoms/Txt'
-import Cloudright from '../../../images/404/cloud_right.png'
-import Cloudleft from '../../../images/404/cloud_left.png'
-import Wavefront from '../../../images/404/wave_front.png'
-import Waveback from '../../../images/404/wave_back.png'
-import Ship from '../../../images/404/ship.png'
-import Ballooning from '../../../images/404/ballooning.png'
-import {navigate} from 'gatsby'
+import Container from '../../Atoms/Container'
+import {navigate, graphql, useStaticQuery} from 'gatsby'
+// import Img from 'gatsby-image'
 
-const Error404 = () => (
-  <div className={styles.root}>
-    <div className={styles.notfound404}>
-      <Heading className={styles.heading}>Page not found 404</Heading>
-      <LongTxt className={styles.sabheading}>
-        わぁ！海で迷子になってしまったみたい <br />
-        ブラウザバックで前のページに戻ってください
-      </LongTxt>
-      <Button className={styles.home} onClick={() => navigate('/')}>
-        HOMEに戻る
-      </Button>
+const Error404 = () => {
+  const illusts = useStaticQuery(
+    graphql`
+      query {
+        allFile(filter: {relativeDirectory: {eq: "404"}}) {
+          nodes {
+            id
+            name
+            publicURL
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <div className={styles.root}>
+      <Container className={styles.notfound404}>
+        <Heading className={styles.heading}>Page not found 404</Heading>
+        <LongTxt className={styles.subheading}>
+          わぁ！海で迷子になってしまったみたい <br />
+          ブラウザバックで前のページに戻ってください
+        </LongTxt>
+        <Button onClick={() => navigate('/')}>HOMEに戻る</Button>
+      </Container>
+      {illusts.allFile.nodes.map(illust => (
+        <div
+          className={[styles.image, styles[illust.name]].join(' ')}
+          key={illust.id}
+        >
+          <img src={illust.publicURL} />
+        </div>
+      ))}
     </div>
-    <img className={styles.ballooning} src={Ballooning} />
-    <img className={styles.cloudleft} src={Cloudleft} />
-    <img className={styles.cloudright} src={Cloudright} />
-    <img className={styles.wavefront} src={Wavefront} />
-    <img className={styles.waveback} src={Waveback} />
-    <img className={styles.ship} src={Ship} />
-  </div>
-)
+  )
+}
 
 export default Error404
