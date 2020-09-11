4b8d375b9dc62d9ada5a10cb58c3c553d4de1374
kuon609 <kuon@prog-8.com>
2020-09-11 18:15:48 +0900

    add notfound stories

---
+++ src/components/Templates/NotFoundTemplate/index.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import NotFoundTemplate from './index'
+
+storiesOf('Template/NotFoundTemplate', module).add('Default', () => (
+  <NotFoundTemplate />
+))
