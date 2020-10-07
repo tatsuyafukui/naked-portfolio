707210cee2d999ad7b16556810c97464bbbe8743
kuon609 <kuon@prog-8.com>
2020-10-07 10:12:59 +0900

    一旦コミット

--- src/components/Organisms/SkillOverall/index.js
+++ src/components/Organisms/SkillOverall/index.js
@@ -1,40 +1,84 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-// import styles from './styles.module.scss'
+import styles from './styles.module.scss'
 import OgpList from '../../Organisms/OgpList'
+import {graphql, useStaticQuery} from 'gatsby'
+import Img from 'gatsby-image'
+import Txt from '../../Atoms/Txt'
+import Heading from '../../Atoms/Heading'
 
 const SkillOverall = ({overall}) => {
   const [active, setActive] = useState('easy')
 
+  const data = useStaticQuery(graphql`
+    {
+      easy: file(relativePath: {eq: "skills/ben_icon.png"}) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      middle: file(relativePath: {eq: "skills/ken_icon.png"}) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
   const tabItems = [
     {
+      id: 1,
       level: 'easy',
       title: '駆け出しエンジニア',
       description: '〇〇なレベル',
+      icon: data.easy.childImageSharp.fluid,
     },
     {
+      id: 2,
       level: 'middle',
       title: 'ジュニアエンジニア',
       description: '〇〇なレベル',
+      icon: data.middle.childImageSharp.fluid,
     },
   ]
 
   return (
     <div>
-      <ul>
+      <div className={styles.tab}>
         {tabItems.map(tabItem => (
-          <li key={tabItem.level} onClick={() => setActive(tabItem.level)}>
-            {tabItem.title}
-          </li>
+          <button
+            className={styles.tabItem}
+            key={tabItem.level}
+            onClick={() => setActive(tabItem.level)}
+          >
+            <div className={styles.tabIconWrap}>
+              <Img
+                className={styles.tabIcon}
+                fluid={tabItem.icon}
+                alt={tabItem.title}
+              />
+              <div className={styles.tabTextWrap}>
+                <Txt className={styles.levelText}>レベル{tabItem.id}</Txt>
+                <Txt>{tabItem.title}</Txt>
+              </div>
+            </div>
+            <Txt>{tabItem.description}</Txt>
+          </button>
         ))}
-      </ul>
+      </div>
       <div>
-        <p>{overall[active].description}</p>
+        <Txt>{overall[active].description}</Txt>
         {overall[active].tasks.map((task, index) => (
-          <div key={index}>
-            <p>{task.title}</p>
+          <div className={styles.taskWrap} key={index}>
+            <Heading className={styles.taskTitle}>{task.title}</Heading>
             {task.child.map((child, index) => (
-              <p key={index}>{child}</p>
+              <Txt className={styles.taskText} key={index}>
+                {child}
+              </Txt>
             ))}
           </div>
         ))}
--- src/components/Organisms/SkillOverall/index.stories.js
+++ src/components/Organisms/SkillOverall/index.stories.js
@@ -2,7 +2,10 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 import SkillOverall from './index'
 import skillData from '../../../mock/data/skill.json'
+import {NarrowedContainer} from '../../Atoms/Container'
 
 storiesOf('Organisms/SkillOverall', module).add('Default', () => (
-  <SkillOverall overall={skillData.skillsJson.overall} />
+  <NarrowedContainer>
+    <SkillOverall overall={skillData.skillsJson.overall} />
+  </NarrowedContainer>
 ))
