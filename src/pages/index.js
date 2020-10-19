8d65836bcf1985828590a913e07a28cff515dc0b
kuon609 <kuon@prog-8.com>
2020-10-19 18:39:07 +0900

    一旦コミット

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,6 @@
 import React from 'react'
 import IndexTemplate from '../components/Templates/IndexTemplate'
-import {graphql, useStaticQuery} from 'gatsby'
 
-const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allScenesJson {
-        nodes {
-          id
-          image {
-            publicURL
-          }
-        }
-      }
-    }
-  `)
-
-  return <IndexTemplate scenes={data.allScenesJson.nodes} />
-}
+const IndexPage = () => <IndexTemplate />
 
 export default IndexPage
