1553da13ce6aefb6966446f1d949e9e7b077f5d5
tatsuyafukui <nakedtatsuya@gmail.com>
2020-11-17 11:53:59 +0900

    delete template storybook

--- src/components/Templates/SkillTemplate/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {storiesOf} from '@storybook/react'
-import SkillTemplate from './index'
-import skillData from '../../../mock/data/skill.json'
-
-storiesOf('Template/SkillTemplate', module).add('Default', () => (
-  <SkillTemplate data={skillData} />
-))
--- src/components/Templates/TrademarkTemplate/index.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import {storiesOf} from '@storybook/react'
-import TrademarkTemplate from './index'
-
-storiesOf('Template/TrademarkTemplate', module).add('Default', () => (
-  <TrademarkTemplate />
-))
